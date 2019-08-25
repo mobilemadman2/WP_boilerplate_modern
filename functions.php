4e46843363482a8f2f1139ce45c25f3dde6ce383
grzegorzboczek <kontakt@pikselowy.pl>
2019-08-25 21:04:46 +0200

    added latest jQuery and Font Awesome 5.10.x

--- functions.php
+++ functions.php
@@ -13,3 +13,23 @@ function load_js() {
 }
 
 add_action( 'wp_enqueue_scripts', 'load_js' );
+
+// include jQuery CDN
+function shapeSpace_include_custom_jquery() {
+
+	wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), null, true);
+
+}
+add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');
+
+// include Font Awesome
+function prefix_enqueue_awesome() {
+	wp_enqueue_style( 
+		'font-awesome-5', 
+		'https://use.fontawesome.com/releases/v5.10.2/css/all.css', 
+		array(), 
+		'5.3.0' 
+	);
+}
+add_action( 'wp_enqueue_scripts', 'prefix_enqueue_awesome' );
