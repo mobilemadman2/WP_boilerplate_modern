ce9cb3e7db4f052e4948ee0030341e3abff26ef7
grzegorzboczek <kontakt@pikselowy.pl>
2019-08-26 09:20:06 +0200

    added php functions in head for charset and language.

--- header.php
+++ header.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+
+    <!-- Detect user language -->
+<html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="UTF-8">
+
+    <!-- Take charset from theme np. UTF8 -->
+    <meta charset="<?php bloginfo('charset'); ?>">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <!-- Take name of theme and insert in title -->
+    <title><?php bloginfo('name'); ?></title>
 
     <!-- Adding all things in head np. CSS.main declarete in functioncs.php -->
     <?php wp_head(); ?>
-    
+
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico">
+
     
 </head>
 
