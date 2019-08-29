09ecf03fb9fd46c9fb22970f1f7d0a55ffd84294
grzegorzboczek <kontakt@pikselowy.pl>
2019-08-29 11:04:25 +0200

    more changes in index.php >loop for metadate

--- index.php
+++ index.php
@@ -7,8 +7,28 @@
         <!-- @@ WordPress LOOP @@ -->
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
+        <!-- Posts iteration -->
+        <article <?php post_class(); ?> >
+
+            <h2> <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <?php the_content(); ?>
+
+            <footer>
+                <p class="byline">
+                    Author: 
+                    <a href="<?php echo get_author_posts_url( $post->post_author ); ?>">
+                        <?php the_author(); ?>
+                    </a> |
+                    Date:
+                    <?php the_time( 'M. j, Y'); ?> |
+                    Categories:
+                    <?php the_category( ','); ?>
+                    Tags:
+                    <?php the_tags( '', ',', '') ?>
+                </p>
+            </footer>
+
+        </article>
 
         <?php endwhile; else: ?>
         <!-- Display 404 error massage with translate option. (TextDomain in style.css) -->
